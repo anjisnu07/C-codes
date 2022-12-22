4eee5439c24393f7d2db5578b1f4f5aab516f00e
Anjisnu Roy <royanjisnu78@gmail.com>
2022-12-22 13:00:21 +0530

    Update selection sort.c

--- DSA/SORTING/selection sort.c
+++ DSA/SORTING/selection sort.c
@@ -1,37 +1,43 @@
-#include <stdio.h>  
-  
-void selection(int arr[], int n)  
-{  
-    int i, j, small;  
-      
-    for (i = 0; i < n-1; i++)    
-    {  
-        small = i; 
-          
-        for (j = i+1; j < n; j++)  
-        if (arr[j] < arr[small])  
-            small = j;  
-    int temp = arr[small];  
-    arr[small] = arr[i];  
-    arr[i] = temp;  
-    }  
-}  
-  
-void printArr(int a[], int n)   
-{  
-    int i;  
-    for (i = 0; i < n; i++)  
-        printf("%d ", a[i]);  
-}  
-  
-int main()  
-{  
-    int a[] = { 12, 31, 25, 8, 32, 17 };  
-    int n = sizeof(a) / sizeof(a[0]);  
-    printf("Before sorting array elements are - \n");  
-    printArr(a, n);  
-    selection(a, n);  
-    printf("\nAfter sorting array elements are - \n");    
-    printArr(a, n);  
-    return 0;  
-}    
+#include<stdlib.h>
+#include<stdio.h>
+int main(){
+	int n,data,i,j,temp,indexofmin;
+	
+	printf("Enter size of array:");
+	scanf("%d",&n);
+	
+	int arr[n];
+	
+	printf("\nEnter array inputs:\n");
+	for(i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	
+	printf("\nArray before sorting:\n");
+		for(i=0;i<n;i++){
+			printf("%d ",arr[i]);
+		}
+// selection sort
+
+for(data=0;data<n-1;data++){
+	
+	indexofmin=data;
+	
+	for(j=data+1;j<n;j++){
+		if(arr[j]<arr[indexofmin]){
+			
+			indexofmin=j;
+			
+		}
+	}
+	
+	temp=arr[data];
+	arr[data]=arr[indexofmin];
+	arr[indexofmin]=temp;
+}
+
+printf("\nArray after sorting:\n");
+		for(i=0;i<n;i++){
+			printf("%d ",arr[i]);
+		}
+}
