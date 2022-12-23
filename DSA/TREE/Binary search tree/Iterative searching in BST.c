d751cdafa497aaa3692b453c7a99e98755c4a2f2
Anjisnu Roy <royanjisnu78@gmail.com>
2022-12-24 01:35:32 +0530

    Add files via upload

---
+++ DSA/TREE/Binary search tree/Iterative searching in BST.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	int data;
+struct node *left;
+struct node *right;	
+};
+
+struct node *createnode(){
+	struct node *temp;
+	temp=(struct node *)malloc(sizeof(struct node));
+	printf("Enter data to insert:");
+	scanf("%d",&temp->data);
+	temp->left=NULL;
+	temp->right=NULL;
+}
+
+void inorder(struct node *root){
+	if(root != NULL){
+		inorder(root->left);
+		printf("%d ",root->data);
+		inorder(root->right);
+	}
+}
+
+struct node *search(struct node *p,int key){
+	
+while( p != NULL){
+
+	
+if(p->data==key){
+	return key;
+}
+else if(key< p->data){
+	p=p->left;
+}
+else{
+	p=p->right;
+}
+	
+	
+}
+
+return NULL;
+}
+
+
+
+
+int main(){
+	
+	int key;
+	
+	struct node *p=createnode();
+	struct node *q=createnode();
+	struct node *r=createnode();
+	struct node *s=createnode();
+	struct node *t=createnode();
+	struct node *u=createnode();
+	
+	p->left=q;
+	p->right=r;
+	
+	q->left=s;
+	q->right=t;
+	
+	r->left=u;
+	
+	 inorder(p);
+
+
+	printf("\nEnter data you wanna search:\n");
+		scanf("%d",&key);	 
+	
+	struct node *v=search(p,key);
+	
+	if(v != NULL){
+		printf("%d Data found ",key);
+	}
+	else{
+		printf("Data isn't in BST.'");
+	}
+	 
+}
